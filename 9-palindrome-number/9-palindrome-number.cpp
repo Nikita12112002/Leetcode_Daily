d4558355d36c0500a616cef29d5a298d96598fbd
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-04-10 14:45:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (33.72%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-     //using extra space;   
-        vector<int>ans;
+        
+        // without using extra space.
         
         if(x<0)
             return false;
         
-        while(x!=0)
+        long long y=x , num=0;
+        
+        while(x>0)
         {
-            int rem=x%10;
-            ans.push_back(rem);
+             num = num*10;
+            num=num+(x%10);
             x=x/10;
         }
         
-        for(int i=0 ;i<ans.size()/2;i++)
-        {
-            if(ans[i]!=ans[ans.size()-1-i])
-                return false;
-        }
-        
-        return true;
+        if(num==y)
+            return true;
+        else
+            return false;
     }
 };
