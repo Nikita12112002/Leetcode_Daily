dfe9d7dbeb916ee7971218bff9672bda3269a076
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-04-13 01:19:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (55.38%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -12,19 +12,35 @@ public:
 //         return climbStairs(n-1)+climbStairs(n-2);
         
         //TABULATION METHOD -DP
-        vector<int>dp(n+1);
-        if(n==0)
-            return 1;
-        if(n==1)
+//         vector<int>dp(n+1);
+//         if(n==0)
+//             return 1;
+//         if(n==1)
+//             return 1;
+        
+//         dp[0]=1;
+//         dp[1]=1;
+//         for(int i=2 ;i<n+1 ;i++)
+//         {
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+        
+//         return dp[n];
+        
+        //SPACE OPTIMIZATION
+        
+        int prev2=1 , prev1=1 ,curr;
+        
+        if(n<=1)
             return 1;
         
-        dp[0]=1;
-        dp[1]=1;
         for(int i=2 ;i<n+1 ;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            curr=prev2+prev1;
+            prev2=prev1;
+            prev1=curr;
         }
         
-        return dp[n];
-    }
+        return curr;
+     }
 };
