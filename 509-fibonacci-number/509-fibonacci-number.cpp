76da2e653f9097c20fbf368499d14caa075c8054
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-04-13 01:03:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (42.10%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -11,20 +11,42 @@ public:
         
         // Now converting into dp code-Tabulation method;
         
-        vector<int>dp(n+1);
-         if(n==0)
-             return 0;
+//         vector<int>dp(n+1);
+//          if(n==0)
+//              return 0;
+//         if(n==1)
+//             return 1;
+        
+//         dp[0]=0;
+//         dp[1]=1;
+        
+//         for(int i=2 ;i<n+1;i++)
+//         {
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+        
+//         return dp[n];
+        
+        //SPACE OPTIMIZATION METHOD
+        
+        if(n==0)
+            return 0;
         if(n==1)
             return 1;
         
-        dp[0]=0;
-        dp[1]=1;
+        int prev2=0 , prev1=1,curr;
         
-        for(int i=2 ;i<n+1;i++)
+        for(int i=2;i<n+1;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            curr = prev1+prev2;
+            
+            prev2=prev1;
+            prev1=curr;
         }
         
-        return dp[n];
-    }
+        return curr;
+        
+	
+}
+    
 };
