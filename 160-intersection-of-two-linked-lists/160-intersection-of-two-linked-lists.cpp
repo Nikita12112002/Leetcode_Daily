d7b5f688099187ca9b9b7c3704fac1beee7aac46
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-05-03 10:27:19 +0530

    Time: 57 ms (57.41%), Space: 14.3 MB (99.04%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -30,28 +30,50 @@ public:
         
         
 //         return NULL;
+ 
+        // optimal using hashmap
+//         unordered_map<ListNode* ,int>m;
         
-        unordered_map<ListNode* ,int>m;
+//         ListNode *temp1=head1;
+//         ListNode *temp2=head2;
+        
+//         while(temp1!=NULL)
+//         {
+            
+//             m[temp1]++;
+//             temp1=temp1->next;
+
+//         }
+        
+//         while(temp2!=NULL)
+//         {
+//             if(m.find(temp2)!=m.end())
+//                     return temp2;
+//                 else
+//                     temp2=temp2->next;
+//         }
+        
+//         return NULL;
+        
+        //best- TC:O(m) SC:O(1)
+        
+        if(head1==NULL || head2==NULL)
+            return NULL;
         
         ListNode *temp1=head1;
         ListNode *temp2=head2;
         
-        while(temp1!=NULL)
+        while(temp1!=temp2)
         {
-            
-            m[temp1]++;
-            temp1=temp1->next;
-
+            if(temp1==NULL)
+                temp1=head2;
+            else
+                temp1=temp1->next;
+            if(temp2==NULL)
+                temp2=head1;
+            else
+                temp2=temp2->next;
         }
-        
-        while(temp2!=NULL)
-        {
-            if(m.find(temp2)!=m.end())
-                    return temp2;
-                else
-                    temp2=temp2->next;
-        }
-        
-        return NULL;
+        return temp1;
     }
 };
