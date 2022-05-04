4e4a98aa46457bb423556909ba45a44e3cb35bbd
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-05-04 08:06:33 +0530

    Added solution - LeetHub

--- Flattening a Linked List - GFG/flattening-a-linked-list.cpp
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -110,87 +110,47 @@ struct Node{
 
 /*  Function which returns the  root of 
     the flattened linked list. */
-//     Node* merge(Node *a , Node *b)
-// {
-// 	Node *temp=new Node(0);
-// 	Node *res=temp;
+    Node* merge(Node* a , Node* b)
+{
+	Node *temp=new Node(0);
+	Node *res=temp;
 	
-// 	while(a!=NULL && b!=NULL)
-// 	{
-// 		if(a->data<b->data)
-// 		{
-// 			temp->bottom=a;
-// 			temp=temp->bottom;
-// 			a=a->bottom;
-// 		}
-// 		else
-// 		{
-// 			temp->bottom=b;
-// 			temp=temp->bottom;
-// 			b=b->bottom;
-// 		}
-// 	}
+	while(a!=NULL && b!=NULL)
+	{
+		if(a->data<b->data)
+		{
+			temp->bottom=a;
+			temp=temp->bottom;
+			a=a->bottom;
+		}
+		else
+		{
+			temp->bottom=b;
+			temp=temp->bottom;
+			b=b->bottom;
+		}
+	}
 	
-// 	while(a!=NULL)
-// 	{
-// 		temp->bottom=a;
-// 	}
-// 	while(b!=NULL)
-// 	{
-// 		temp->bottom=b;
-// 	}
-// 	return res->bottom;
-// }
-
-// Node *flatten(Node *root)
-// {
-//   // Your code here
-//   if(root==NULL || root->next==NULL)
-// 		return root;
-	
-// 	root->next=flatten(root->next);
-	
-// 	root=merge(root,root->next);
-// 	return root;
-// }
-
-Node* mergeTwoLists(Node* a, Node* b) {
-    
-    Node *temp = new Node(0);
-    Node *res = temp; 
-    
-    while(a != NULL && b != NULL) {
-        if(a->data < b->data) {
-            temp->bottom = a; 
-            temp = temp->bottom; 
-            a = a->bottom; 
-        }
-        else {
-            temp->bottom = b;
-            temp = temp->bottom; 
-            b = b->bottom; 
-        }
-    }
-    
-    if(a) temp->bottom = a; 
-    else temp->bottom = b; 
-    
-    return res -> bottom; 
-    
+	if(a!=NULL)
+	{
+		temp->bottom=a;
+	}
+	if(b!=NULL)
+	{
+		temp->bottom=b;
+	}
+	return res->bottom;
 }
+
 Node *flatten(Node *root)
 {
-   
-        if (root == NULL || root->next == NULL) 
-            return root; 
-  
-        // recur for list on right 
-        root->next = flatten(root->next); 
-  
-        // now merge 
-        root = mergeTwoLists(root, root->next); 
-  
-        // return the root 
-        // it will be in turn merged with its left 
-        return root; 
+  // Your code here
+  if(root==NULL || root->next==NULL)
+		return root;
+	
+	root->next=flatten(root->next);
+	
+	root=merge(root,root->next);
+	return root;
 }
+
