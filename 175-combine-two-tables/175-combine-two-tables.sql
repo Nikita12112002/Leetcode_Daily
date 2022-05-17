9a800ff4b0475e2d984dc52bd87cfa46b568e98b
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-05-17 21:22:39 +0530

    Time: 479 ms (24.08%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -3,4 +3,3 @@
 select Person.firstName , Person.lastName , Address.city ,Address.state
 from Person left join Address on person.personId=Address.personId;
 
-# SELECT Person.FirstName, Person.LastName, Address.City, Address.State from Person LEFT JOIN Address on Person.PersonId = Address.PersonId;
