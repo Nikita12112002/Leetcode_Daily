7771df8bb25380b96bee79ca99b037e6cd9e1557
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-05-25 07:23:41 +0530

    Added solution - LeetHub

--- Find Nth root of M - GFG/find-nth-root-of-m.cpp
+++ Find Nth root of M - GFG/find-nth-root-of-m.cpp
@@ -6,61 +6,40 @@ using namespace std;
 class Solution{
 	public:
 	
-// 		int multi(int number , int n)
-// 	{
-// 	    int ans=1;
-// 	    for(int i=1 ;i<=n;i++)
-// 	    {
-// 	        ans=ans*number;
-// 	    }
+		int multi(int number , int n)
+	{
+	    int ans=1;
+	    for(int i=1 ;i<=n;i++)
+	    {
+	        ans=ans*number;
+	    }
 	    
-// 	    return ans;
-// 	}
+	    return ans;
+	}
 	
 	int NthRoot(int n, int m)
 	{
 	    
-	    // Code here.
-	   // int low=1 , high=m;
+	   // Code here.
+	    int low=1 , high=m;
 	    
-	   // while(low<=high)
-	   // {
-	   //     int mid = (low+high)/2;
+	    while(low<=high)
+	    {
+	        int mid = (low+high)/2;
 	        
-	   //     if(multi(mid,n)==m)
-	   //       return mid;
+	        if(pow(mid,n)==m)
+	          return mid;
 	        
-	   //     if(multi(mid,n)<m)
-	   //      low=mid+1;
+	        if(pow(mid,n)<m)
+	         low=mid+1;
 	         
-	   //      else
-	   //       high=mid-1;
-	   // }
+	         else
+	          high=mid-1;
+	    }
 	    
-	   // return -1;
+	    return -1;
+	}    
 	    
-	     // Code here.
-    int start=1;
-    int end=m;
-    while(start<=end)
-    {
-        int mid=(start+end)/2;
-        if(pow(mid,n)==m)
-        {
-            return mid;
-        }
-        else if(pow(mid,n)>m)
-        {
-            end=mid-1;
-        }
-        else if(pow(mid,n)<m)
-        {
-            start=mid+1;
-        }
-         
-    }
-    return -1;
-	}  
 	
 
 };
