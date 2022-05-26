3612027cad2d2e63f11e13b7dd773788f675e65c
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-05-26 16:28:26 +0530

    Time: 39 ms (37.86%), Space: 22.4 MB (55.74%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-      //brute force sol -- using xor   
-        int ans=nums[0];
+      //note--
+      //right half--> 1st instance-odd index
+      //               2nd instance--even index
         
-        for(int i=1 ;i<nums.size() ;i++)
+       //left half--> 1st instance-->even index
+       //             2nd instance-->odd index
+        int low=0 ,high=nums.size()-2;
+        
+        while(low<=high)
         {
-            ans=ans^nums[i];
+           int mid=(low+high)/2;
+            
+        // note- xor of even index with 1 always gives odd index
+        // and xor of odd index with 1 always give even index
+            
+            if(nums[mid]==nums[mid^1]) // signifies we are on the left half
+            // mid^1 is going to give previous even or next odd
+                low=mid+1;
+            else
+                high=mid-1;
         }
-        return ans;
+        return nums[low];
+        
     }
 };
