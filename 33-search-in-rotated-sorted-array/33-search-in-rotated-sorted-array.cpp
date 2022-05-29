d8356360a1c8b9ff7eb48534193ca3bfd28d8e67
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-05-29 12:49:55 +0530

    Time: 9 ms (24.34%), Space: 11.1 MB (75.83%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int low=0 , high=nums.size()-1;
+        
+        while(low<=high)
+        {
+            int mid= (low+high)/2; 
+            
+            if(nums[mid]==target)
+                return mid;
+            
+            else if(nums[low]<=nums[mid]) // left part is sorted
+            {
+                //check wheather target belongs to left part
+                if(target>=nums[low] && target<=nums[mid]) 
+                    high=mid-1;
+                
+                else
+                    low=mid+1; // means left part is sorted but target doesnot belong to left part
+            }
+            // left part is unsorted it means right part will defintely be sorted so we update low;
+            else
+            {
+                //check the target in the right part
+                if(target>=nums[mid] && target<=nums[high]) 
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+            
+        }
+       return -1; 
+    }
+};
