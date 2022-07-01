84eeda096cbe9ecc6b6dd37f85c255f5d453364b
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-01 23:26:02 +0530

    Time: 110 ms (39.78%), Space: 32.3 MB (77.01%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -28,13 +28,7 @@ public:
            
           return (int)((long)p * q % 1000000007);
         
-        // int maxh = max(hc[0], h - hc.back()),
-        //     maxv = max(vc[0], w - vc.back());
-        // for (int i = 1; i < hc.size(); i++)
-        //     maxh = max(maxh, hc[i] - hc[i-1]);
-        // for (int i = 1; i < vc.size(); i++)
-        //     maxv = max(maxv, vc[i] - vc[i-1]);
-        // return (int)((long)maxh * maxv % 1000000007);
+       
         
     }
 };
