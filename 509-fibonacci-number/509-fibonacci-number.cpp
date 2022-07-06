e026d8fdea585dfc91c3c92b0544efcf8013c2f3
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-06 09:56:41 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,52 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-       //This is the recursive code
         
-        // if(n==0)
-        //     return 0;
-        // if(n==1)
-        //     return 1;
-        // return fib(n-1)+fib(n-2);
-        
-        // Now converting into dp code-Tabulation method;
-        
-//         vector<int>dp(n+1);
-//          if(n==0)
-//              return 0;
-//         if(n==1)
-//             return 1;
-        
-//         dp[0]=0;
-//         dp[1]=1;
-        
-//         for(int i=2 ;i<n+1;i++)
-//         {
-//             dp[i]=dp[i-1]+dp[i-2];
-//         }
-        
-//         return dp[n];
-        
-        //SPACE OPTIMIZATION METHOD
+        vector<int>dp(n+1);
         
         if(n==0)
             return 0;
         if(n==1)
             return 1;
+        dp[0]=0;
+        dp[1]=1;
         
-        int prev2=0 , prev1=1,curr;
-        
-        for(int i=2;i<n+1;i++)
+        for(int i=2 ;i<n+1 ;i++)
         {
-            curr = prev1+prev2;
-            
-            prev2=prev1;
-            prev1=curr;
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        
-        return curr;
-        
-	
-}
-    
+        return dp[n];
+    }
 };
