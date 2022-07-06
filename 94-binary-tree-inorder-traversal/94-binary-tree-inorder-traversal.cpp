5c8aabaeb7976275deb878c2edf7ef27ae1ab242
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-06 21:35:30 +0530

    Time: 3 ms (59.62%), Space: 8.4 MB (40.90%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -14,20 +14,31 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         
         vector<int>in;
-        inorder(root,in);
+       
+        stack<TreeNode*>st;
+        
+        
+        while(true)
+        {
+            if(root!=NULL)
+            {
+                st.push(root);
+                root=root->left;
+            }
+            
+            else
+            {
+                if(st.empty()==true)
+                    break;
+                
+                root =st.top();
+                st.pop();
+                
+                in.push_back(root->val);
+                root=root->right;
+            }
+        }
         return in;
-        
-    }
-    
-    void inorder(TreeNode* root ,vector<int>&in)
-    {
-        if(root==NULL)
-            return;
-        
-        inorder(root->left , in);
-        in.push_back(root->val);
-        inorder(root->right ,in);
-        
         
     }
 };
