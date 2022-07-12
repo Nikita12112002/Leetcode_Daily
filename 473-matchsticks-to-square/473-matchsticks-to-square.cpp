2df23200ee0b1213395d9d61f7ff9b3b22b33170
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-12 15:21:04 +0530

    Time: 404 ms (48.66%), Space: 64.4 MB (7.46%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool makesquare(vector<int>& match) {
+     
+     int n = match.size(),sum=0;
+        
+     if(n<4)
+         return false;
+        
+     for(int i=0 ;i<n;i++)
+         sum=sum+match[i];
+    
+     int perimeter = sum/4;
+        
+      vector<int>sides(4,0);
+      sort(match.begin(),match.end(),greater<int>());  
+      return dfs(match,sides,perimeter,0);
+    }
+    
+    bool dfs(vector<int>match , vector<int>&sides ,int perimeter, int index)
+    {
+      if(index==match.size()) 
+      {
+          if(sides[0]== sides[1] && sides[1]==sides[2] && sides[2]==sides[3])
+              return true;
+          return false;
+      }
+      
+        for(int i=0 ;i<4;i++)
+        {
+            if(sides[i]+match[index]>perimeter)
+                continue;
+            
+            int j=i-1;
+            while(j>=0)
+            {
+                if(sides[j]==sides[i])
+                    break;
+                j--;
+            }
+            
+            if(j!=-1)
+                continue;
+            sides[i]=sides[i]+match[index];
+            
+            if(dfs(match,sides,perimeter,index+1))
+                return true;
+            
+            sides[i]=sides[i]-match[index];
+        }
+        return false;
+    }
+};
