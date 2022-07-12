04e7adc3e0e65f6680799dabef813bc2a0ac0638
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-12 20:48:05 +0530

    Time: 62 ms (32.73%), Space: 32.7 MB (78.17%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* helper(TreeNode* root)
+    {
+        if(root->left==NULL)
+            return root->right;
+        
+        if(root->right==NULL)
+            return root->left;
+        
+        TreeNode* rightChild = root->right;
+        TreeNode* rightMostChildOfLeft = findRightMost(root->left);
+        
+        rightMostChildOfLeft->right = rightChild;
+        
+        return root->left;
+
+    }
+    
+    TreeNode* findRightMost(TreeNode* root)
+    {
+        while(root->right)
+        {
+            root=root->right;
+        }
+        return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+        if(root==NULL)
+            return NULL;
+        
+        if(root->val==key)
+            return helper(root);
+        
+        TreeNode* dummy=root;
+        
+        while(root!=NULL)
+        {
+            if(root->val>key)
+            {
+                if(root->left!=NULL && root->left->val==key)
+                {
+                    root->left=helper(root->left);
+                    break;
+                }
+                else
+                    root=root->left;
+            }
+            else
+            {
+                if(root->right && root->right->val==key)
+                {
+                    root->right=helper(root->right);
+                    break;
+                }
+                else
+                    root=root->right;
+            }
+
+        }
+        return dummy;
+    }
+};
