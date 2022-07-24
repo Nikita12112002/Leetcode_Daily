3fad548f8a37932c9d0ad108af390a32f81b6e26
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-24 19:06:54 +0530

    Time: 26 ms (11.14%), Space: 6 MB (39.74%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,19 +2,11 @@ class Solution {
 public:
     int fib(int n) {
         
-        vector<int>dp(n+1);
-        
         if(n==0)
             return 0;
         if(n==1)
             return 1;
-        dp[0]=0;
-        dp[1]=1;
+        return fib(n-1)+fib(n-2);
         
-        for(int i=2 ;i<n+1 ;i++)
-        {
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
     }
 };
