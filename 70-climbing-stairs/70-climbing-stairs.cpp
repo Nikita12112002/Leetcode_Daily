5a2d5a6c3434616573bb62dbbd8118bee5c5c3c6
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-25 17:58:07 +0530

    Time: 3 ms (35.83%), Space: 6.2 MB (15.07%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        
-        //Since we have count no of ways we can thought of recursion
-        
-//         if(n==0)
-//             return 1;
-//         if(n==1)
-//          return 1;
-        
-//         return climbStairs(n-1)+climbStairs(n-2);
-        
-        //TABULATION METHOD -DP
-//         vector<int>dp(n+1);
-//         if(n==0)
-//             return 1;
-//         if(n==1)
-//             return 1;
-        
-//         dp[0]=1;
-//         dp[1]=1;
-//         for(int i=2 ;i<n+1 ;i++)
-//         {
-//             dp[i]=dp[i-1]+dp[i-2];
-//         }
-        
-//         return dp[n];
-        
-        //SPACE OPTIMIZATION
-        
-        int prev2=1 , prev1=1 ,curr;
-        
-        if(n<=1)
+    
+    int f(int n , vector<int>&dp)
+    {
+        if(n==0 || n==1)
             return 1;
         
-        for(int i=2 ;i<n+1 ;i++)
-        {
-            curr=prev2+prev1;
-            prev2=prev1;
-            prev1=curr;
-        }
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]= f(n-1,dp)+f(n-2,dp);
+    }
+    int climbStairs(int n) {
+    
+       if(n==0|| n==1)
+           return 1;
+        vector<int>dp(n+1,-1);
+       return  f(n,dp);
         
-        return curr;
-     }
+    }
 };
