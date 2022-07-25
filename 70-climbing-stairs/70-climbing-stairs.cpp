1987501a9a1392b274e3e67600dfc42d245e0540
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-25 18:04:30 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.07%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -6,9 +6,13 @@ public:
         if(n==0 || n==1)
             return 1;
         
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]= f(n-1,dp)+f(n-2,dp);
+        dp[0]=dp[1]=1;
+        
+        for(int i=2 ;i<=n ;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
     int climbStairs(int n) {
     
