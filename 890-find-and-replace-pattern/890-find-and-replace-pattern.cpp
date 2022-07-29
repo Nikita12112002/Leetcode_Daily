7bd3fcb225e8a90cbfb8abbb1c7a472776860594
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-07-29 20:34:19 +0530

    Time: 12 ms (18.71%), Space: 8.6 MB (43.97%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        
+        int n=pattern.length();
+        unordered_map<char,char>m1;
+        unordered_map<char,char>m2;
+        int flag=1;
+        vector<string>ans;
+        
+        for(int i=0 ;i<words.size() ;i++)
+        {
+           map<char,char>m1;
+            map<char,char>m2;
+            flag=1;
+            for(int j=0 ; j<words[i].size() ;j++)
+            {
+                auto it1= m1.find(pattern[j]);
+                auto it2 =m2.find(words[i][j]);
+                
+                if(it1==m1.end())
+                  m1.insert({pattern[j] , words[i][j]});
+                else
+                {
+                    if(it1->second!=words[i][j])
+                    {
+                        flag=0;
+                        break;
+                    }
+                }
+                
+                if(it2==m2.end())
+                  m2.insert({words[i][j] , pattern[j]});
+                else
+                {
+                    if(it2->second!=pattern[j])
+                    {
+                        flag=0;
+                        break;
+                    }
+                }
+        }
+        
+            
+           
+            
+        if(flag)
+            ans.push_back(words[i]);
+        }
+        return ans;
+        
+    }
+};
