c19fa31e78524cec2615c9eedfb3dac837369db4
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-08-03 20:28:00 +0530

    Time: 291 ms (25.61%), Space: 73 MB (46.54%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    int dfs(string &s , string &t , int i , int j , vector<vector<int>>&dp)
-    {
-        if(i<0 || j<0)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(s[i]==t[j])
-            return dp[i][j] = 1+dfs(s,t,i-1,j-1,dp);
-        return dp[i][j]=max(dfs(s,t,i-1,j,dp) , dfs(s,t,i,j-1,dp));
-    }
+    
     int longestPalindromeSubseq(string s) {
         
         string t=s;
@@ -17,8 +8,23 @@ public:
         
         int n = s.size();
         int m=n;
-        vector<vector<int>>dp(n,vector<int>(m+1,-1));
-        return dfs(s,t,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         
+        for(int i=0 ; i<=n ;i++)
+            dp[i][0]=0;
+        for(int i=0 ; i<=m ; i++)
+            dp[0][i]=0;
+        
+        for(int i=1 ; i<=n ; i++)
+        {
+            for(int j=1 ; j<=m ;j++)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j]= 1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
