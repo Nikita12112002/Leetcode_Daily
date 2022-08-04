ce3f5c06ff33160cacc3f685cf542b014a74840a
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-08-04 11:06:54 +0530

    Time: 84 ms (24.45%), Space: 18.5 MB (48.68%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int dfs(string &s , string &t , int i , int j , vector<vector<int>>&dp)
-    {
-        if(j<0)
-            return 1;
-        if(i<0)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        if(s[i]==t[j])
-            return dp[i][j]=dfs(s,t,i-1,j-1,dp)+dfs(s,t,i-1,j,dp);
-       
-            return dp[i][j]=dfs(s,t,i-1,j,dp);
-    }
-    int numDistinct(string s, string t) {
+    int mod=1e9+7;
+   int numDistinct(string s, string t) {
         
         int n= s.length();
         int m = t.length();
-        vector<vector<int>>dp(n,vector<int>(m+1,-1));
-        return dfs(s,t,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         
+        for(int i=0 ; i<=n ;i++)
+            dp[i][0]=1;
+     
+        for(int i=1 ; i<=n ;i++)
+        {
+            for(int j=1 ;j<=m ;j++)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j]= (dp[i-1][j-1]+dp[i-1][j])%mod;
+                else
+                    dp[i][j]=dp[i-1][j]%mod;
+            }
+        }
+        return dp[n][m];
+       
+       
     }
 };
