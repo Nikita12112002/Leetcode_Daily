b650e30fffc01b88c7b564cc0ca664023b6cde68
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-08-08 13:01:46 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (13.15%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int dfs(vector<int>&nums , int i, int prev , int d , vector<vector<int>>&dp , int n)
-    {
-        if(i==n)
-            return 0;
-        if(nums[i]-nums[prev]!=d)
-            return 0;
-        if(dp[i][prev]!=-1)
-            return dp[i][prev];
-        return dp[i][prev]= 1+dfs(nums,i+1,i,d,dp,n);
-    }
+   
     int numberOfArithmeticSlices(vector<int>& nums) {
         
         int n=nums.size();
-        int count=0;
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        for(int i=0 ; i<n-2 ;i++)
+        int sum=0;
+        vector<int>dp(n,0);
+       
+        for(int i=2 ; i<n ;i++)
         {
-            int d= nums[i+1] - nums[i];
-            count=count+dfs(nums,i+2,i+1,d,dp,n);
+            int d= nums[i-1] - nums[i-2];
+            if(nums[i]-nums[i-1]==d)
+                dp[i]= 1+dp[i-1];
         }
-        return count;
+       
+        for(int i=0 ; i<n ;i++)
+            sum = sum + dp[i];
+        return sum;
     }
 };
