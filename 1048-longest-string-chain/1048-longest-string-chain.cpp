8d8a2b3ecbe978d1f083fe180de3a5ca20d05ab2
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-08-09 12:02:50 +0530

    Time: 177 ms (39.80%), Space: 13.2 MB (90.49%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -27,26 +27,7 @@ public:
         return a.size()<b.size();
     }
     
-//     bool compare_strings(string& s1, string& s2){
-//     if(s1.size() != s2.size() + 1) return false;
-    
-//     int first = 0;
-//     int second = 0;
-    
-//     while(first < s1.size()){
-//         if(second < s2.size() && s1[first] == s2[second]){
-//             first ++;
-//             second ++;
-//         }
-//         else first ++;
-//     }
-//     if(first == s1.size() && second == s2.size()) return true;
-//     else return false; 
-// }
 
-// bool static comp(string& s1, string& s2){
-//     return s1.size() < s2.size();
-// }
 
     int longestStrChain(vector<string>& words) {
         
