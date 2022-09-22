0e5c0d07311ed882766de4ccd10c00f4e6fc0c24
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-09-22 12:23:57 +0530

    Time: 919 ms (19.88%), Space: 120 MB (22.22%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-//      int dfs(vector<int>&nums , vector<int>&multipliers , int n , int m , int i , int j , int k , vector<vector<int>>&dp)
-//         {
+     int dfs(vector<int>&nums , vector<int>&multipliers , int n , int m , int i , int j , int k , vector<vector<int>>&dp)
+        {
          
-//          // right pointer can be said as ->suppose right is 1 so we can say last from starting so right is dependent on start we can express it in terms of start.
-//             if(k==m)
-//                 return 0;
+         // right pointer can be said as ->suppose right is 1 so we can say last from starting so right is dependent on start we can express it in terms of start.
+            if(k==m)
+                return 0;
             
-//            if(dp[k][i]!=-1)
-//                return dp[k][i];
-//             int left = nums[i]*multipliers[k]+dfs(nums,multipliers,n,m,i+1,j,k+1,dp);
-//             int right=nums[j]*multipliers[k]+dfs(nums,multipliers,n,m,i,j-1,k+1,dp);
+           if(dp[k][i]!=INT_MIN)
+               return dp[k][i];
+            int left = nums[i]*multipliers[k]+dfs(nums,multipliers,n,m,i+1,j,k+1,dp);
+            int right=nums[j]*multipliers[k]+dfs(nums,multipliers,n,m,i,j-1,k+1,dp);
             
-//             return dp[k][i]=max(left,right);
-//         }
+            return dp[k][i]=max(left,right);
+        }
     
     
-       int dfs(vector<int>& nums,vector<int>& multi,int n , int m, int i , int j , int k ,vector<vector<int>> &dp){
-        if(k==multi.size()) return 0;
-        if(dp[k][i]!=INT_MIN) return dp[k][i];
-        int ans=INT_MIN;
-        ans= max(multi[k]*nums[i]+ dfs(nums,multi,n,m,i+1,j,k+1,dp),
-                multi[k]*nums[j]+ dfs(nums,multi,n,m, i,j-1,k+1,dp));
-        return dp[k][i]= ans;
-    }
+   
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
         
          
