9b13f6a3d20751593edc6ca9342a2c02b439136e
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-09-25 12:59:25 +0530

    Time: 255 ms (34.76%), Space: 61.3 MB (62.08%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -5,13 +5,14 @@ public:
         int slow = nums[0];
         int fast = nums[0];
         
-      do
+         slow=nums[slow];
+         fast=nums[nums[fast]];
+        
+        while(slow!=fast)
         {
             slow=nums[slow];
             fast=nums[nums[fast]];
-            
         }
-        while(slow!=fast);
         fast=nums[0];
         while(slow!=fast)
         {
