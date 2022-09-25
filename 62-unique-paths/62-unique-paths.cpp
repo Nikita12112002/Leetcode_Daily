8b35fe1a92d3a537333bfe9806ad9fb6986f71ea
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-09-25 07:15:37 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (27.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    int dfs(int i , int j ,vector<vector<int>>&dp)
+    {
+        if(i==0 && j==0)
+            return 1;
+        if(i<0 || j<0)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j]=dfs(i-1,j,dp)+dfs(i,j-1,dp);
+    }
+
     int uniquePaths(int m, int n) {
         
-        // combatorics solution
-        
-        int N=m+n-2;
-        int r=m-1;
-        long long res=1;
-        for(int i=1 ;i<=r ;i++)
-        {
-            res=res*(N-r+i)/i;
-        }
-        return (int)res;
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return dfs(m-1,n-1,dp);
     }
 };
