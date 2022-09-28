6814cdec7fd68e025e8899a7201aad659904d837
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-09-28 18:20:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.58%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    bool rotateString(string A, string B) {
+    bool rotateString(string s, string goal) {
         
-            if(A==B)return true; // if already same
-      
-      
-      if(A.size()!=B.size())return false; //if size aren't equal
-      
-      
-     A=A+A; //will have all the possible rotation now 
-      
-      int check=A.find(B); //will find the B substring
-      
-        if(check>=A.size())return false; //if the position is greater then size
-      
-  return true;
+        if(s==goal)
+            return true;
+        if(s.length()!=goal.length())
+            return false;
+          s=s+s;
+            
+            int index = s.find(goal);
+            
+            if(index>s.size())
+                return false;
+            
+        
+       return true; 
+        
         
     }
+    
+    
+     
 };
