40e723c8562335d3f6dea5e4f2fff657a44fa253
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-09-28 17:15:21 +0530

    Time: 10 ms (69.28%), Space: 9 MB (97.44%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -2,55 +2,55 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         
-//         int n = nums.size();
+        long long n = nums.size();
         
      
             
         
-//         int first=LONG_MIN , second=LONG_MIN , third=LONG_MIN;
-//         for(int i=0 ;i<n ;i++)
-//         {
-//             if (nums[i] == first || nums[i] == second || nums[i] == third)
-//                 continue;
-//             if(nums[i]>first)
-//             {
-//                 third=second;
-//                 second=first;
-//                 first=nums[i];
-//             }
-            
-//             else if(nums[i]>second)
-//             {
-                
-//                 third=second;
-//                 second=nums[i];
-//             }
-//             else if(nums[i]>third)
-//             {
-//                 third=nums[i];
-//             }
-//         }
-//        return third == LONG_MIN ? first : third;
-//     }
-    
-    
-        long one = LONG_MIN, two = LONG_MIN, three = LONG_MIN;
-        for (int num : nums) {
-            if (num == one || num == two || num == three)
+        long long first=LONG_MIN , second=LONG_MIN , third=LONG_MIN;
+        for(long long i=0 ;i<n ;i++)
+        {
+            if (nums[i] == first || nums[i] == second || nums[i] == third)
                 continue;
+            if(nums[i]>first)
+            {
+                third=second;
+                second=first;
+                first=nums[i];
+            }
             
-            if (num > one) {
-                three = two;
-                two = one;
-                one = num;
-            } else if (num > two) {
-                three = two;
-                two = num;
-            } else if (num > three) {
-                three = num;
+            else if(nums[i]>second)
+            {
+                
+                third=second;
+                second=nums[i];
+            }
+            else if(nums[i]>third)
+            {
+                third=nums[i];
             }
         }
-        
-        return three == LONG_MIN ? one : three;
+       return third == LONG_MIN ? first : third;
     }
+    
+    
+//         long one = LONG_MIN, two = LONG_MIN, three = LONG_MIN;
+//         for (int num : nums) {
+//             if (num == one || num == two || num == three)
+//                 continue;
+            
+//             if (num > one) {
+//                 three = two;
+//                 two = one;
+//                 one = num;
+//             } else if (num > two) {
+//                 three = two;
+//                 two = num;
+//             } else if (num > three) {
+//                 three = num;
+//             }
+//         }
+        
+//         return three == LONG_MIN ? one : three;
+//     }
 };
