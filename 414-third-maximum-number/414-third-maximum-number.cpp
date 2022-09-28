b32969ba2fdbf6653e9fd8fa084ccf3e986d4ebe
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-09-28 17:15:37 +0530

    Time: 6 ms (87.86%), Space: 9.1 MB (84.52%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -34,23 +34,5 @@ public:
     }
     
     
-//         long one = LONG_MIN, two = LONG_MIN, three = LONG_MIN;
-//         for (int num : nums) {
-//             if (num == one || num == two || num == three)
-//                 continue;
-            
-//             if (num > one) {
-//                 three = two;
-//                 two = one;
-//                 one = num;
-//             } else if (num > two) {
-//                 three = two;
-//                 two = num;
-//             } else if (num > three) {
-//                 three = num;
-//             }
-//         }
-        
-//         return three == LONG_MIN ? one : three;
-//     }
+
 };
