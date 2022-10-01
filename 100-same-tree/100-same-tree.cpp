1b0828ab9c5cbb0082451d579a1ae181bd560e86
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-10-01 18:57:28 +0530

    Time: 4 ms (46.72%), Space: 10.1 MB (44.49%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -16,9 +16,6 @@ public:
         if(p==NULL || q==NULL)
             return p==q;
         
-        return(p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right , q->right));
-     
-        
-        
+        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
