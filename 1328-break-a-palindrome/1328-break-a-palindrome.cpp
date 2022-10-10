eb9f4a0356e14f8ba4eef2db5ae03f500ee71e87
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-10-10 17:50:22 +0530

    Time: 3 ms (45.61%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,25 @@
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.size();
+        
+        if (n == 1) return "";
+        
+      
+        
+        for (int i = 0; i < n / 2; i++) {
+            
+            if (palindrome[i] != 'a') {
+               
+                palindrome[i] = 'a';
+              
+                return palindrome;
+            }
+        }
+       
+        palindrome[n - 1] = 'b';
+        return palindrome;
+    }
+};
