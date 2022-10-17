3692a03f2c130fa82fb2c33289970ee66cae6d4c
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-10-17 11:28:51 +0530

    Time: 98 ms (20.91%), Space: 21.9 MB (72.04%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int largestPerimeter(vector<int>& A) {
+    int largestPerimeter(vector<int>& nums) {
         
-         sort(begin(A), end(A));
-  for (auto i = A.size() - 1; i >= 2; --i)
-    if (A[i] < A[i - 1] + A[i - 2]) 
-        return A[i] + A[i - 1] + A[i - 2];
-  return 0;
+        sort(nums.begin(),nums.end(),greater<int>());
         
+        for(int i=0 ;i<=nums.size()-3;i++)
+        {
+           if(nums[i+2]+nums[i+1]>nums[i])
+               return nums[i]+nums[i+1]+nums[i+2];
+           
+             
+        }
+        
+        return 0;
     }
 };
