f0790c44d9197ac60e8c5ff8a8af2ac7fab2e1df
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-10-29 20:20:50 +0530

    Time: 348 ms (91.37%), Space: 168.6 MB (29.92%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     int earliestFullBloom(vector<int>& plant, vector<int>& grow) {
         
-        
+        int start=0;
         vector<pair<int, int>> v;
-    for (auto i = 0; i < plant.size(); ++i)
-        v.push_back({grow[i], plant[i]});
-    sort(begin(v), end(v));
-    int res = 0;
-    for (auto it : v)
-        res = max(res, it.first) + it.second;
+        
+       for (auto i = 0; i < plant.size(); i++)
+       {
+            v.push_back({grow[i], plant[i]});
+       }
+        
+     sort(v.begin(), v.end(), greater<pair<int,int>>());
+        int res = 0;
+        for (auto it : v)
+        {
+            start = start+it.second;
+            res = max(res,start+it.first);
+        }
     return res;
     }
 };
