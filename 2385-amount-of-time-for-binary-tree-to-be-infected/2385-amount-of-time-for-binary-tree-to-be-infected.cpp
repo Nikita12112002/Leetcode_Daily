50fde956c4bd778a44aea0421e905a614a1be896
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-10-29 22:23:51 +0530

    Time: 626 ms (75.41%), Space: 154.3 MB (61.65%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,32 +11,7 @@
  */
 class Solution {
 public:
-//     TreeNode* markParents(TreeNode* root , int start ,unordered_map<TreeNode*,TreeNode*>m , queue<TreeNode*>q)
-//     {
-//         q.push(root);
-//         TreeNode* res;
-//         while(!q.empty())
-//         {
-//             auto node = q.front();
-            
-//             if(node->val==start)
-//                 res=node;
-                
-//             q.pop();
-            
-//             if(node->left)
-//             {
-//                 q.push(node->left);
-//                 m[node->left]=node;
-//             }
-//             if(node->right)
-//             {
-//                 q.push(node->right);
-//                 m[node->right]=node;
-//             }
-//         }
-//         return res;
-//     }
+
       TreeNode* markParents(TreeNode* root , int start , unordered_map<TreeNode*,TreeNode*>&par)
   {
       queue<TreeNode*>q;
@@ -67,53 +42,8 @@ public:
      return res; 
   }
     int amountOfTime(TreeNode* root, int start) {
-        
-//         queue<TreeNode*>q;
-//         unordered_map<TreeNode*,TreeNode*>m1;
-//         TreeNode* res = markParents(root,start,m1,q);
-        
-//         queue<TreeNode*>q2;
-//         unordered_map<TreeNode*, bool>m2;
-//         int time=0,flag=0;
-//         q2.push(res);
-//         m2[res]=true;
-//         while(!q2.empty())
-//         {
-//             int sz = q2.size();
-//             flag=0;
-            
-//             for(int i=0 ;i<sz ;i++)
-//             {
-//             auto node = q2.front();
-//             q2.pop();
-            
-//             if(node->left && m2[node->left]==false)
-//             {
-//                 flag=1;
-//                 q2.push(node->left);
-//                 m2[node->left]=true;
-//             }
-            
-//             if(node->right && m2[node->right]==false)
-//             {
-//                 flag=1;
-//                 q2.push(node->right);
-//                 m2[node->right]=true;
-//             }
-            
-//             if(m1[node] && m2[m1[node]]==false)
-//             {
-//                 q2.push(m1[node]);
-//                 m2[m1[node]]=true;
-                
-//             }
-//             } 
-//             if(flag==1)
-//                 time++;
-//         }
-//         return time;
-        
-          unordered_map<TreeNode*,TreeNode*>par;
+
+      unordered_map<TreeNode*,TreeNode*>par;
       TreeNode* target = markParents(root,start,par);
         unordered_map<TreeNode*,bool>visited;
         queue<TreeNode*>q;
