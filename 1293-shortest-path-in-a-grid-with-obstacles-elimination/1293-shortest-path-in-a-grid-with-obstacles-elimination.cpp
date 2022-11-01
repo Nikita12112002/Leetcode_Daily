dfcf62bd66b7ef24c0a8dc9641a7047e9a27b2db
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-11-01 21:25:07 +0530

    Time: 774 ms (9.50%), Space: 25 MB (61.07%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    int dfs(vector<vector<int>>&grid , int k  , int i , int j,  vector<vector<vector<int>>> &dp, vector<vector<bool>> &visited)
+    int dfs(vector<vector<int>>&grid , int k  , int i , int j,  vector<vector<vector<int>>> &dp)
     {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || visited[i][j])
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==2)
             return 1e9;
         
          if(dp[i][j][k]!=-1)
@@ -20,14 +20,16 @@ public:
            
         }
         
+        int c= grid[i][j];
+        grid[i][j]=2;
        
-        visited[i][j]=true;
-         int top = dfs(grid, k, i-1, j,dp,visited);
-        int left = dfs(grid, k, i, j-1,dp,visited);
-        int down = dfs(grid, k, i+1, j,dp,visited);
-        int right = dfs(grid, k, i, j+1,dp,visited);
-        visited[i][j] = false;
-
+        int top = dfs(grid, k, i-1, j,dp);
+        int left = dfs(grid, k, i, j-1,dp);
+        int down = dfs(grid, k, i+1, j,dp);
+        int right = dfs(grid, k, i, j+1,dp);
+       
+        grid[i][j]=c;
+        
         return dp[i][j][k] = 1+min(top, min(left, min(right, down)));
     }
     int shortestPath(vector<vector<int>>& grid, int k) {
@@ -35,8 +37,8 @@ public:
         int n = grid.size();
         int m = grid[0].size();
     vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k+1, -1)));
-         vector<vector<bool>> visited(n, vector<bool>(m, false));
-        int res = dfs(grid,k,n-1,m-1,dp,visited);
+        
+        int res = dfs(grid,k,n-1,m-1,dp);
         
 
         
