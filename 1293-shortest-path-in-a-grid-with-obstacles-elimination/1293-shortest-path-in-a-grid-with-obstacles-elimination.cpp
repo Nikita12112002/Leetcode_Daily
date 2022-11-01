d8b36ca367730474712a8ecce0069c2986f8ed5e
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-11-01 21:32:16 +0530

    Time: 610 ms (12.10%), Space: 25.2 MB (60.94%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    int dfs(vector<vector<int>>&grid , int k  , int i , int j,  vector<vector<vector<int>>> &dp)
+    int dfs(vector<vector<int>>&grid , int k  , int i , int j,  vector<vector<vector<int>>> &dp, vector<vector<bool>> &visited)
     {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==2)
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || visited[i][j])
             return 1e9;
         
          if(dp[i][j][k]!=-1)
@@ -20,29 +20,32 @@ public:
            
         }
         
-        int c= grid[i][j];
-        grid[i][j]=2;
        
-        int top = dfs(grid, k, i-1, j,dp);
-        int left = dfs(grid, k, i, j-1,dp);
-        int down = dfs(grid, k, i+1, j,dp);
-        int right = dfs(grid, k, i, j+1,dp);
-       
-        grid[i][j]=c;
+        visited[i][j]=true;
+        int up = dfs(grid,k,i-1,j,dp,visited);
+          int left = dfs(grid,k,i,j-1,dp,visited);
+        int down = dfs(grid,k,i+1,j,dp,visited);
+      
+        int right = dfs(grid,k,i,j+1,dp,visited);
         
-        return dp[i][j][k] = 1+min(top, min(left, min(right, down)));
+       visited[i][j]=false;
+        
+       int ans1= min(up,down);
+        int ans2= min(left,right);
+        
+           return dp[i][j][k] =1+ min(up, min(down, min(left, right)));
     }
     int shortestPath(vector<vector<int>>& grid, int k) {
         
         int n = grid.size();
         int m = grid[0].size();
     vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k+1, -1)));
-        
-        int res = dfs(grid,k,n-1,m-1,dp);
+         vector<vector<bool>> visited(n, vector<bool>(m, false));
+        int res = dfs(grid,k,n-1,m-1,dp,visited);
         
 
         
-        if(res >= 1e9)
+        if(res >= m*n)
             return -1;
         return res;
     }
