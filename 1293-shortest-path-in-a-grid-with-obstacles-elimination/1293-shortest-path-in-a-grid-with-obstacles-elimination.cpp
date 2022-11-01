eb4a5b72748a23e17518b113c14b0793b85e646c
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-11-01 21:22:04 +0530

    Time: 447 ms (21.17%), Space: 25.2 MB (60.94%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,95 +1,49 @@
-// class Solution {
-// public:
-    
-//     int dfs(vector<vector<int>>&grid , int k  , int i , int j,   vector<vector<bool>>&visited)
-//     {
-//         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() ||  visited[i][j])
-//             return 1e9;
-//         else if(i==0 && j==0)
-//             return 0;
-        
-//         else if(grid[i][j]==1)
-//         {
-//             if(k==0)
-//                 return 1e9;
-//             k--;
-           
-//         }
-        
-       
-//        visited[i][j] = true;
-        
-//         int up = 1+dfs(grid,k,i-1,j,visited);
-//         int down = 1+dfs(grid,k,i+1,j,visited);
-//         int left = 1+dfs(grid,k,i,j-1,visited);
-//         int right = 1+dfs(grid,k,i,j+1,visited);
-        
-//        visited[i][j] = false;
-//       return min(up, min(down, min(left, right)));
-//     }
-//     int shortestPath(vector<vector<int>>& grid, int k) {
-        
-//         int n = grid.size();
-//         int m = grid[0].size();
-        
-//        vector<vector<bool>> visited(m, vector<bool>(n, false));
-//         int res = dfs(grid,k,n-1,m-1,visited);
-        
-//         if(res >= 1e9)
-//             return -1;
-//         return res;
-//     }
-            
-
-// };
-
-
-
-
 class Solution {
 public:
-    int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k+1, -1)));
-        int ans = path(grid, k, m-1, n-1,visited, dp);
-        if(ans >= m*n)
-            return -1;
-        return ans;
-    }
-
-    int path(vector<vector<int>>& grid, int k, int r, int c,vector<vector<bool>>& visited, vector<vector<vector<int>>>& dp){
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if(r >= m || c >= n || r < 0 || c < 0 || visited[r][c]){
-            return 1e6;
-        }
-
-        if(dp[r][c][k] != -1){
-            return dp[r][c][k];
+    
+    int dfs(vector<vector<int>>&grid , int k  , int i , int j,  vector<vector<vector<int>>> &dp, vector<vector<bool>> &visited)
+    {
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || visited[i][j])
+            return 1e9;
+        
+         if(dp[i][j][k]!=-1)
+            return dp[i][j][k];
+        
+        if(i==0 && j==0)
+            return dp[i][j][k]=0;
+        
+         if(grid[i][j]==1)
+        {
+            if(k==0)
+                return dp[i][j][k]=1e9;
+            k--;
+           
         }
         
-        if(r == 0 && c == 0){
-            return dp[r][c][k] = 0;
-        }
+       
+        visited[i][j]=true;
+         int top = 1+dfs(grid, k, i-1, j,dp,visited);
+        int left = 1+dfs(grid, k, i, j-1,dp,visited);
+        int down = 1+dfs(grid, k, i+1, j,dp,visited);
+        int right = 1+dfs(grid, k, i, j+1,dp,visited);
+        visited[i][j] = false;
 
-        if(grid[r][c]){
-            if(k == 0){
-                return dp[r][c][k] = 1e6;
-            }
-            else{
-                k--;
-            }
-        }
-        visited[r][c] = true;
-        int top = 1+path(grid, k, r-1, c,visited, dp);
-        int left = 1+path(grid, k, r, c-1,visited, dp);
-        int down = 1+path(grid, k, r+1, c,visited, dp);
-        int right = 1+path(grid, k, r, c+1,visited, dp);
-        visited[r][c] = false;
-
-        return dp[r][c][k] = min(top, min(left, min(right, down)));
+        return dp[i][j][k] = min(top, min(left, min(right, down)));
     }
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+    vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k+1, -1)));
+         vector<vector<bool>> visited(n, vector<bool>(m, false));
+        int res = dfs(grid,k,n-1,m-1,dp,visited);
+        
+
+        
+        if(res >= 1e9)
+            return -1;
+        return res;
+    }
+            
+
 };
