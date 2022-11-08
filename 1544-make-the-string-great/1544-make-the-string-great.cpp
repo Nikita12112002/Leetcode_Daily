3827fcc444cb078efef49b4add03104dc2e2e048
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-11-08 11:58:38 +0530

    Time: 2 ms (84.38%), Space: 6.6 MB (26.42%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     string makeGood(string s) {
-       int n=s.size();
-					
-					if(s.size()==0)
-						return "";
-					if(s.size()==1)
-						return s;
-					for(int i=0;i<s.size();++i)
-					{
-						if(abs(s[i]-s[i+1])==32)
-						{
-							s.erase(i,2);
-                            cout<<s<<" ";
-							if(i==0)
-								i--;
-							else
-								i-=2;
-                            
-						}    
-					}
-					return s;
+      
+	    stack<char> st;
+        for(auto i: s){
+            if(st.empty()){
+                st.push(i);
+            }
+            else{
+                if(abs(i-st.top()) == 32){
+                    st.pop();
+                }
+                else{
+                    st.push(i);
+                }
+            }
+            
+        }
+        string ans;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    
     }
     
 };
