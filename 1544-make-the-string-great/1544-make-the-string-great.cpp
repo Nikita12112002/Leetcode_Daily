9bf674e4c43b9acc3ef2e9198900640585b40852
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-11-08 10:55:40 +0530

    Time: 5 ms (54.29%), Space: 6.2 MB (92.00%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string makeGood(string s) {
+       int n=s.size();
+					
+					if(s.size()==0)
+						return "";
+					if(s.size()==1)
+						return s;
+					for(int i=0;i<s.size();++i)
+					{
+						if(abs(s[i]-s[i+1])==32)
+						{
+							s.erase(i,2);
+							if(i==0)
+								i--;
+							else
+								i-=2;
+						}    
+					}
+					return s;
+    }
+    
+};
