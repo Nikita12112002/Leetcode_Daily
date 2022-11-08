d38daaaf25d444d8f032c47800fc013f94069c35
Nikita Gupta <68268617+Nikita12112002@users.noreply.github.com>
2022-11-08 11:29:27 +0530

    Time: 12 ms (9.35%), Space: 6.3 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -12,10 +12,12 @@ public:
 						if(abs(s[i]-s[i+1])==32)
 						{
 							s.erase(i,2);
+                            cout<<s<<" ";
 							if(i==0)
 								i--;
 							else
 								i-=2;
+                            
 						}    
 					}
 					return s;
